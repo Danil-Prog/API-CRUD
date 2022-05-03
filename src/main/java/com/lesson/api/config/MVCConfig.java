4d7c38f4900146791e7a9306845587402b7a7ea3
Danil Gavrilov <dany.gavrik@mail.ru>
2022-05-03 22:19:50 +0700

    Add favicon

--- src/main/java/com/lesson/api/config/MVCConfig.java
+++ src/main/java/com/lesson/api/config/MVCConfig.java
@@ -1,7 +1,10 @@
 package com.lesson.api.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,6 +18,27 @@ public class MVCConfig implements WebMvcConfigurer {
         registry.addViewController("/login").setViewName("login/login");
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/");
+
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/static/js/");
+
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations("classpath:/static/img/");
+
+    }
+
+
+    @Controller
+    static class FaviconController {
+        @RequestMapping("favicon.ico")
+        String favicon() {
+            return "forward:/img/favicon.ico";
+        }
+    }
 
 
 }
