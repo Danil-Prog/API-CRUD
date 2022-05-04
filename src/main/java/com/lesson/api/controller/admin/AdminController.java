819a96e6a300ec73169beb34a54059b1dd01bb39
Danil Gavrilov <dany.gavrik@mail.ru>
2022-05-04 22:26:33 +0700

    Info authenticated user for admin page

--- src/main/java/com/lesson/api/controller/admin/AdminController.java
+++ src/main/java/com/lesson/api/controller/admin/AdminController.java
@@ -1,9 +1,13 @@
 package com.lesson.api.controller.admin;
 
 
+import com.lesson.api.entity.User;
 import com.lesson.api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.AuthenticatedPrincipal;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -13,7 +17,10 @@ public class AdminController {
     private UserService userService;
 
     @GetMapping("/admin/v1/users")
-    public String userList(){
+    public String userList(@AuthenticationPrincipal User user, Model model){
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("user_date_registration", user.getDate());
+        model.addAttribute("user_role", user.getRoles());
         return "admin/users";
     }
 
